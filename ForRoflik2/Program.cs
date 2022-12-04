9d33507bd4307ead634a3efb2f9aadd33df53398
PROGTIME-SCHOOL <117563363+PROGTIME-SCHOOL@users.noreply.github.com>
2022-12-04 12:18:53 +0300

    Change

--- ForRoflik2/Program.cs
+++ ForRoflik2/Program.cs
@@ -6,9 +6,9 @@ namespace SumAdd
     {
         static void Main(string[] args)
         {
-            int a1 = int.Parse(Console.ReadLine());
-            int a2 = int.Parse(Console.ReadLine());
-            Console.WriteLine(a1+a2);
+            //int a1 = int.Parse(Console.ReadLine());
+            //int a2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine(a1+a2);
         }
     }
 }
