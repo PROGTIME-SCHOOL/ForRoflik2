293de540de860f3217992fed690948639635a11f
andrey738 <76277376+andrey738@users.noreply.github.com>
2022-12-04 11:56:26 +0300

    Initial commit

---
+++ ForRoflik2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SumAdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a1 = int.Parse(Console.ReadLine());
+            int a2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(a1+a2);
+        }
+    }
+}
